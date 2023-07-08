0924eaecf5080bdbba272bb66f081cabc315a1a7
unknown <nandagb23@gmail.com>
2023-07-08 11:38:13 -0300

    adicionando menu de cadastro

---
+++ SuperTickets/src/View/Menu.java
@@ -0,0 +1,62 @@
+package View;
+
+import java.util.Scanner;
+
+public class Menu {
+    Scanner scanner = new Scanner(System.in);
+    public void show(){
+        System.out.println("Bem vindo ao SuperTickets!");
+        System.out.println("Já possui um cadastro? (s/n)");
+        String opcao = scanner.nextLine();
+        switch(opcao){
+            case "s":
+                this.login();
+                break;
+            case "n":
+                this.cadastrar();
+                break;
+        }
+    }
+
+    public void login(){
+
+    }
+
+    public void cadastrarUsuario(){
+        System.out.println("Preencha seus dados:");
+        System.out.println("Nome: ");
+        scanner.nextLine();
+        String nome = scanner.nextLine();
+        System.out.println("Email: ");
+        String email = scanner.nextLine();
+        System.out.println("Senha: ");
+        String senha = scanner.nextLine();
+    }
+
+    public void cadastrar(){
+        System.out.println("Qual o seu objetivo com o SuperTickets?");
+        System.out.println("Digite o valor correspondente: ");
+        System.out.println("[1] Comprar Tickets para eventos\n[2] Organizar eventos");
+        int opcao = scanner.nextInt();
+        switch(opcao){
+            case 1:
+                this.cadastrarComprador();
+                break;
+            case 2:
+                this.cadastrarOrganizador();
+                break;
+        }
+    }
+
+    public void cadastrarComprador(){
+        this.cadastrarUsuario();
+        System.out.println("cpf: ");
+        String cpf = scanner.nextLine();
+    }
+
+    public void cadastrarOrganizador(){
+        this.cadastrarUsuario();
+        System.out.println("cnpj: ");
+        String cpf = scanner.nextLine();
+    }
+}
