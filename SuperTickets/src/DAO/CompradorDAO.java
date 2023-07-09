a8df7fcbaac3ad63a55cb6b9005024f93428558f
unknown <nandagb23@gmail.com>
2023-07-08 23:55:06 -0300

    adicionando compradorDAO

--- SuperTickets/src/DAO/CompradorDAO.java
+++ SuperTickets/src/DAO/CompradorDAO.java
@@ -2,26 +2,105 @@ package DAO;
 
 
 import Entity.Comprador;
+import Entity.Usuario;
+import db.DBConnection;
+import db.DBDados;
+import db.DBDriver;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CompradorDAO {
+
+    Connection con;
+    DBDados dados = new DBDados();
+
+    public CompradorDAO() {
+        if (con == null) {
+            con = new DBConnection().getConnection(DBDriver.MYSQL, dados.getSchema(), dados.getUser(), dados.getPassword());
+        }
+    }
+
+    public PreparedStatement buildFullStatementSave(PreparedStatement pst, Comprador comprador) throws SQLException{
+        pst.setInt(1, comprador.getId());
+        pst.setInt(2, comprador.getCpf());
+        return pst;
+    }
+
+    public Comprador buildUsuario(ResultSet rs) throws SQLException{
+        Comprador comprador = new Comprador();
+        comprador.setId(rs.getInt("usuario_id_comprador"));
+        comprador.setId(rs.getInt("cpf"));
+
+        return comprador;
+    }
+
     public boolean save(Comprador comprador) throws SQLException {
+        String query = "INSERT INTO comprador (usuario_id_comprador, cpf) VALUES (?, ?)";
+        PreparedStatement pst;
+        pst = con.prepareStatement(query);
+        pst = buildFullStatementSave(pst, comprador);
+
+        int res = pst.executeUpdate();
+
+        if (res == 1) {
+            return true;
+        }
+
         return false;
     }
+
     public boolean update(Comprador comprador) throws SQLException{
         return false;
     }
     public Comprador getById(int id) throws SQLException{
-        Comprador comprador = new Comprador();
-        return comprador;
+        String query = "SELECT * FROM comprador";
+        PreparedStatement ps;
+        ps = con.prepareStatement(query);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            Comprador comprador = buildUsuario(rs);
+            if (comprador.getId() == id) {
+                return comprador;
+            }
+
+        }
+        return null;
     }
     public Comprador getByCpf(int cpf) throws SQLException{
-        Comprador comprador = new Comprador();
-        return comprador;
+        String query = "SELECT * FROM comprador";
+        PreparedStatement ps;
+        ps = con.prepareStatement(query);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            Comprador comprador = buildUsuario(rs);
+            if (comprador.getCpf() == cpf) {
+                return comprador;
+            }
+
+        }
+        return null;
     }
 
     public boolean delete(Comprador comprador) throws SQLException{
+        String string = "DELETE FROM comprador WHERE id_usuario = ?";
+        PreparedStatement pst;
+        try {
+            pst = con.prepareStatement(string);
+            pst.setInt(1, comprador.getId());
+            int res = pst.executeUpdate();
+            if(res == 1){
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return false;
     }
+    
 }
